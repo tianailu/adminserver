cf16ece7f169081b2d491c966ac15969a50c081c
SubaruRabbit <SubaruRabbitOrz@gmail.com>
2023-07-27 01:51:29 +0800

    add log

--- api/admin/auth/init.go
+++ api/admin/auth/init.go
@@ -2,13 +2,19 @@ package auth
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 )
 
 func init() {
 }
 
 func InitTable() {
-	go createTable()
+	go func() {
+		err := createTable()
+		if err != nil {
+			log.Errorf("Failed to init table in auth module, error: %s", err)
+		}
+	}()
 }
 
 func InitRouter(g *echo.Group) {
--- api/admin/user/init.go
+++ api/admin/user/init.go
@@ -1,9 +1,16 @@
 package user
 
+import "github.com/labstack/gommon/log"
+
 func init() {
 	go dealUserData()
 }
 
 func InitTable() {
-	go createTable()
+	go func() {
+		err := createTable()
+		if err != nil {
+			log.Errorf("Failed to init table in user module, error: %s", err)
+		}
+	}()
 }
