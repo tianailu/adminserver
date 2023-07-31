04868916e94fb3cc69fffde0ee6255f476fd10f0
SubaruRabbit <SubaruRabbitOrz@gmail.com>
2023-08-01 01:33:42 +0800

    update user model

--- api/admin/user/data.go
+++ api/admin/user/data.go
@@ -10,58 +10,70 @@ import (
 
 type (
 	User struct {
-		Id               int     `json:"id" gorm:"primaryKey;autoIncrement;not null;comment:主键"`
-		AccountId        string  `json:"account_id" gorm:"size:32;comment:账号ID"`
-		Uid              string  `json:"uid" gorm:"size:32;not null;comment:用户ID"`
-		Name             string  `json:"name" gorm:"size:12;comment:用户名"`
-		Img              string  `json:"img" gorm:"size:128;comment:个人头像"`
-		Gender           int8    `json:"gender" gorm:"not null;default:0;comment:性别，取值为[0:男, 1:女]"`
-		Constellation    string  `json:"constellation" gorm:"size:12;comment:星座"`
-		Birthday         int64   `json:"birthday" gorm:"comment:出生时间，时间戳，单位毫秒"`
-		Height           float32 `json:"height" gorm:"comment:身高，单位cm"`
-		Weight           float32 `json:"weight" gorm:"comment:体重，单位kg"`
-		Hometown         string  `json:"hometown" gorm:"size:12;comment:家乡"`
-		Education        string  `json:"education" gorm:"size:12;comment:最高学历"`
-		School           string  `json:"school" gorm:"size:32;comment:毕业院校"`
-		Work             string  `json:"work" gorm:"size:20;comment:职业"`
-		Company          string  `json:"company" gorm:"size:20;comment:公司"`
-		Income           string  `json:"income" gorm:"size:20;comment:收入"`
-		MobilePhone      string  `json:"mobile_phone" gorm:"size:12;comment:手机号码"`
-		WeChat           string  `json:"wechat" gorm:"size:20;comment:微信号"`
-		Marriage         string  `json:"marriage" gorm:"size:12;comment:婚姻状态"`
-		Habit            string  `json:"habit" gorm:"size:64;comment:生活习惯"`
-		ConsumptionView  string  `json:"consumption_view" gorm:"size:64;comment:消费观"`
-		Family           string  `json:"family" gorm:"size:64;comment:家庭背景"`
-		Interest         string  `json:"interest" gorm:"size:64;comment:兴趣爱好"`
-		LoveView         string  `json:"love_view" gorm:"size:64;comment:爱情观"`
-		TargetAppearance string  `json:"ta_appearance" gorm:"size:64;comment:希望另一半的样子"`
-		BeImpressed      string  `json:"be_impressed" gorm:"size:64;comment:对方什么最能打动自己"`
-		TargetAge        string  `json:"ta_age" gorm:"size:12;comment:希望另一半身高范围"`
-		TargetHeight     string  `json:"ta_height" gorm:"size:12;comment:希望另一半身高范围"`
-		TargetCity       string  `json:"ta_city" gorm:"size:12;comment:希望另一半所在城市"`
-		TargetHometown   string  `json:"ta_hometown" gorm:"size:12;comment:希望另一半的家乡"`
-		TargetEducation  string  `json:"ta_education" gorm:"size:12;comment:希望另一半最低学历"`
-		TargetMarriage   string  `json:"ta_marriage" gorm:"size:12;comment:希望另一半婚姻状态"`
-		VipLevel         int8    `json:"vip_level" gorm:"comment:会员等级"`
-		IdCard           string  `json:"id_card" gorm:"size:18;comment:身份证"`
-		RealName         string  `json:"real_name" gorm:"size:20;comment:真实姓名"`
-		IsRealNameAuth   int8    `json:"is_rn_auth" gorm:"default:0;comment:是否完成实名认证，0:未认证，1:已通过认证"`
-		WorkAuthImg      string  `json:"work_auth_img" gorm:"size:128;comment:工作认证图片"`
-		IsWorkAuth       int8    `json:"is_work_auth" gorm:"default:0;comment:是否完成工作认证，0:未认证，1:已通过认证"`
-		EduAuthImg       string  `json:"edu_auth_img" gorm:"size:128;comment:学历认证图片"`
-		IsEduAuth        int8    `json:"is_edu_auth" gorm:"default:0;comment:是否完成学历认证，0:未认证，1:已通过认证"`
-		Status           int8    `json:"status" gorm:"not null;default:0;comment:未审核，1:已审核，2:vip"`
-		RegisterPlace    string  `json:"register_place" gorm:"size:12;comment:注册地"`
-		RegisterSource   int8    `json:"register_source" gorm:"comment:注册来源，取值为[0:未知, 1:APP, 2:小程序, 3:群组, 4:二维码, 5:管理后台]"`
-		CreatedAt        int64   `json:"created_at" gorm:"comment:注册时间，单位毫秒"`
-		UpdateAt         int64   `json:"update_at" gorm:"comment:修改时间，单位毫秒"`
+		Id             uint         `json:"id" gorm:"primaryKey;autoIncrement;not null;comment:主键"`
+		AccountId      string       `json:"account_id" gorm:"size:32;comment:账号ID"`
+		Uid            string       `json:"uid" gorm:"size:32;not null;comment:用户ID"`
+		Name           string       `json:"name" gorm:"size:12;comment:用户名"`
+		Avatar         string       `json:"avatar" gorm:"size:128;comment:个人头像"`
+		Gender         int8         `json:"gender" gorm:"not null;default:0;comment:性别，取值为[0:男, 1:女]"`
+		Birthday       sql.NullTime `json:"birthday" gorm:"type:datetime;comment:出生日期"`
+		Constellation  string       `json:"constellation" gorm:"size:12;comment:星座"`
+		Height         float32      `json:"height" gorm:"comment:身高，单位cm"`
+		Weight         float32      `json:"weight" gorm:"comment:体重，单位kg"`
+		Education      string       `json:"education" gorm:"size:12;comment:最高学历"`
+		School         string       `json:"school" gorm:"size:32;comment:毕业院校"`
+		Work           string       `json:"work" gorm:"size:12;comment:职业"`
+		Company        string       `json:"company" gorm:"size:20;comment:公司"`
+		Income         string       `json:"income" gorm:"size:20;comment:年收入"`
+		Residence      string       `json:"residence" gorm:"size:12;comment:现居住地（国家地理编码）"`
+		Hometown       string       `json:"hometown" gorm:"size:12;comment:家乡（国家地理编码）"`
+		MobilePhone    string       `json:"mobile_phone" gorm:"size:12;comment:手机号码"`
+		IdentityTag    int8         `json:"identity_tag" gorm:"not null;default=0;comment:身份标签，取值为[0:未选择, 1:母胎单身, 2:未婚单身, 3:离异无孩, 4:离异带孩, 5:离异不带孩, 6:丧偶]"`
+		VipTag         int8         `json:"vip_tag" gorm:"not null;default:0;comment:vip标签"`
+		Status         int8         `json:"status" gorm:"not null;default:0;comment:用户状态，取值为[0:待审（首次申请审核）, 1: 再审核（非首次申请审核）, 2:通过, 3:不通过]"`
+		RegisterPlace  string       `json:"register_place" gorm:"size:12;comment:注册地"`
+		RegisterSource int8         `json:"register_source" gorm:"comment:注册来源，取值为[0:未知, 1:APP, 2:小程序, 3:群组, 4:二维码, 5:管理后台]"`
+		DurationOfUse  int64        `json:"duration_of_use" gorm:"comment:使用时长，单位秒"`
+		CreatedAt      time.Time    `json:"created_at" gorm:"comment:注册时间"`
+		UpdateAt       time.Time    `json:"update_at" gorm:"comment:修改时间"`
+
+		WeChat   string `json:"wechat" gorm:"size:20;comment:微信号"`
+		Marriage string `json:"marriage" gorm:"size:12;comment:婚姻状态"`
+
+		IdCard         string `json:"id_card" gorm:"size:18;comment:身份证"`
+		RealName       string `json:"real_name" gorm:"size:20;comment:真实姓名"`
+		IsRealNameAuth int8   `json:"is_rn_auth" gorm:"default:0;comment:是否完成实名认证，0:未认证，1:已通过认证"`
+		WorkAuthImg    string `json:"work_auth_img" gorm:"size:128;comment:工作认证图片"`
+		IsWorkAuth     int8   `json:"is_work_auth" gorm:"default:0;comment:是否完成工作认证，0:未认证，1:已通过认证"`
+		EduAuthImg     string `json:"edu_auth_img" gorm:"size:128;comment:学历认证图片"`
+		IsEduAuth      int8   `json:"is_edu_auth" gorm:"default:0;comment:是否完成学历认证，0:未认证，1:已通过认证"`
 	}
 
-	Hometown struct {
-		Country    string `json:"country"`
-		Province   string `json:"province"`
-		City       string `json:"city"`
-		AddrDetail string `json:"addr_detail"`
+	AboutMe struct {
+		Id               uint      `json:"id" gorm:"primaryKey;autoIncrement;not null;comment:主键"`
+		UserId           uint      `json:"user_id" gorm:"comment:用户唯一id"`
+		Habit            string    `json:"habit" gorm:"size:64;comment:生活习惯"`
+		ConsumptionView  string    `json:"consumption_view" gorm:"size:64;comment:消费观"`
+		Family           string    `json:"family" gorm:"size:64;comment:家庭背景"`
+		Interest         string    `json:"interest" gorm:"size:64;comment:兴趣爱好"`
+		LoveView         string    `json:"love_view" gorm:"size:64;comment:爱情观"`
+		TargetAppearance string    `json:"ta_appearance" gorm:"size:64;comment:希望另一半的样子"`
+		BeImpressed      string    `json:"be_impressed" gorm:"size:64;comment:对方什么最能打动自己"`
+		CreatedAt        time.Time `json:"created_at" gorm:"comment:创建时间"`
+		UpdateAt         time.Time `json:"update_at" gorm:"comment:修改时间"`
+	}
+
+	MatchSetting struct {
+		Id              uint      `json:"id" gorm:"primaryKey;autoIncrement;not null;comment:主键"`
+		UserId          uint      `json:"user_id" gorm:"comment:用户唯一id"`
+		TargetAge       string    `json:"ta_age" gorm:"size:12;comment:希望另一半身高范围"`
+		TargetHeight    string    `json:"ta_height" gorm:"size:12;comment:希望另一半身高范围"`
+		TargetCity      string    `json:"ta_city" gorm:"size:12;comment:希望另一半所在城市"`
+		TargetHometown  string    `json:"ta_hometown" gorm:"size:12;comment:希望另一半的家乡"`
+		TargetEducation string    `json:"ta_education" gorm:"size:12;comment:希望另一半最低学历"`
+		TargetMarriage  string    `json:"ta_marriage" gorm:"size:12;comment:希望另一半婚姻状态"`
+		CreatedAt       time.Time `json:"created_at" gorm:"comment:创建时间"`
+		UpdateAt        time.Time `json:"update_at" gorm:"comment:修改时间"`
 	}
 
 	VipLevel struct {
